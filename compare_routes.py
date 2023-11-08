1bb9982bd9b891f19e073789817826edca3a353d
Rebecca W Silverstein <rswhipple@gmail.com>
2023-11-08 07:26:57 -0800

    updated

--- compare_routes.py
+++ compare_routes.py
@@ -1,15 +1,8 @@
-# CREATE TABLE routes (
-#    id serial PRIMARY KEY,
-#    points geometry(MultiPoint, 4326)
-#    total_time integer,
-#    total_miles integer,
-# );
-
 from dotenv import load_dotenv
 load_dotenv()
 
 from classes import *
-
+import requests
 import os
 
 from supabase import create_client
@@ -21,9 +14,11 @@ key = os.environ.get("SUPABASE_KEY")
 
 supabase = create_client(url, key)
 
+
 def compare_routes(order_id):
   # get pickup and dropoff points from orders table
   order_table = "orders" 
+
   query = f"""
     SELECT pickup, drop_off, order_vol, order_weight
     FROM {order_table}
@@ -44,7 +39,11 @@ def compare_routes(order_id):
       capacity = check_capacity(order, results)
 
       if capacity:
-        route_id = capacity[0]["route_id"]
+        route_id = results[0]["route_id"]
+
+        # Update orders table with route_id
+        order_update = supabase.table(order_table).update({"order_route_id": route_id}).eq("id", order_id).execute()
+        assert len(order_update.data) > 0, "Error: Unable to update orders table with route_id"
         return route_id
       else:
         return None
@@ -106,3 +105,28 @@ def check_capacity(order, results):
   
   else:
     print(response.error)
+
+
+# pickup and dropoff points come from orders table
+def import_route(points):
+  # Mapbox public token
+  access_token = "pk.eyJ1IjoicnN3aGlwcGxlIiwiYSI6ImNsb2RlbnN0eTA2bnoyaXQ4aWc1YmF0eGgifQ.nVC4l7HRRRiAYT-A_4ySuA"
+
+  # Convert each point into a string (longitude, latitude) format
+  coordinates = ';'.join([f"lon, lat" for lon, lat in points])
+  
+  # Construct the API request URL
+  url = f"https://api.mapbox.com/directions/v5/mapbox/driving/{coordinates}?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token={access_token}"
+
+  # Make the API request
+  response = requests.get(url)
+
+  # Check if the request was successful 
+  if response.status_code == 200:
+    # Return route_data as a json object
+    route_data = response.json()
+    return route_data
+  else:
+    print("Error: Unable to fetch route data")
+
+
