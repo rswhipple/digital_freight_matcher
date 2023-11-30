67b8719d15fd6c7402e30353737a2ae786a88454
Tony Bronzo <3690643+thetone@users.noreply.github.com>
2023-11-29 21:35:50 -0600

    create_new_route() ready

--- tony_funcs.py
+++ tony_funcs.py
@@ -11,41 +11,30 @@ def add_order_to_route(order_id):  # TODO how to update table?
     pass
 
 
-# "create_new_route" function, creates a new route
-    # receives order_id 
-    # create a new Route object
-    # add pickup and drop_off points from the order, all points = [Atlanta, pickup, drop_off, Atlanta]
-    # call "import_route" using new route_id and points
-    # add RouteClass variable to routes table
-    # assign route_id to order_id in orders table
-    # update capacity table with route_geom (using Mapbox API)
-    # update total_miles and total_time in routes table (using Mapbox API)
-    # return route_id
-#*** tony's function, needs to return route_id ***
-            #response_data = {
-            #    "volume": volume,
-            #    "weight": weight,
-            #    "package_type": package_type,
-            #    "pick_up": pick_up,
-            #    "drop_off": drop_off,
-            #    "in_range": True,
-            #}
-# return value will go into is_profitable
 def create_new_route(order_id, order_data):
+    """creates a new route in database
+
+    order_id: int -- order id for needing new route
+    order_data: dict -- data of order for new route
+
+    return: id of new route created
+    """
+    # build new route
     home_base = (-84.3875298776525, 33.754413815792205)
     points = [home_base, order_data["pick_up"], order_data["drop_off"], home_base]
     new_route = {"points": points}
 
-    response = supabase.table('routes').insert(new_route).execute()
-    route_id = response.data[0]["id"]
+    # insert into 'routes' table
+    route_id = supabase.table('routes').insert(new_route).execute()
+    route_id = route_id.data[0]["id"]
 
+    # calculate data
     route_data = import_route(points)
-    response = supabase.table('capacity').insert(new_route).execute()
 
-    new_route.pickup = order_id.start
-    new_route.dropoff = order_id.end
-    # TODO update tables
-    return new_route.id
+    # insert into 'capacity' table
+    response = supabase.table('capacity').insert(route_data).execute()
+
+    return route_id
 
 
 def calculate_price(order_id):
