2306687df0731ed9e4e8c455e7651f0762e568fd
Rebecca W Silverstein <rswhipple@gmail.com>
2023-12-02 14:53:24 -0800

    refactor

--- app/update_original_routes.py
+++ app/update_original_routes.py
@@ -11,11 +11,11 @@ supabase = create_client(something.url, something.something)
 def update_original_routes():
     # route data
     route_data = {
-        "1": (-84.3875298776525, 33.754413815792205),
-        "2": (-85.11039247022218, 34.9161210050057),
-        "3": (-80.9773396382228, 33.4676716195606),
-        "4": (80.9773396382228, ),
-        "5": (volume),
+        "1": (-85.1103924702221, 34.9161210050057),
+        "2": (-81.8920767938344, 33.4676716195606),
+        "3": (-80.9773396382228, 32.0815296895872),
+        "4": (-84.1807668794164, 31.5770410650746),
+        "5": (-85.1587927831466, 32.4661710120819),
     }
     # build new route
     home_base = (-84.3875298776525, 33.754413815792205)
@@ -33,6 +33,8 @@ def update_original_routes():
             updated_route = supabase.table('routes').update(route_row_data) \
                 .eq('id', route_id).execute()
         except:
+            print(f"update original routes: table could not be updated")
+            exit(1)
 
 
 
